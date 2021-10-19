b86e27dc9a08efa0bd0bd4882333d5b89613faac
crinaldi44 <91025658+crinaldi44@users.noreply.github.com>
2021-10-19 14:19:12 -0400

    Resolved CE-9
    Implemented transparent-to-solid style navbar, resolved CE-9 in which mobile buttons were mis-aligned.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,28 @@
 import logo from '../images/logo.svg'
-import React from 'react'
+import React, {useState} from 'react'
 import {Link} from 'react-router-dom'
 import './Navbar.css'
 import Button from './Button'
-import Header from './Banner'
 
 const Navbar = () => {
+
+    //Represents whether we are using the solid navbar state.
+    const [navSolid, setNavSolid] = useState(false);
+
+    //Toggles a background when we scroll past a certain amount.
+    const toggleBackground = () => {
+        if (window.scrollY >= 80) {
+            setNavSolid(true);
+        } else {
+            setNavSolid(false);
+        }
+    }
+    
+    window.addEventListener('scroll', toggleBackground)
+
     return (
         <>
-            <nav>
+            <nav className={navSolid ? 'navbar active' : 'navbar'}>
                     <Link to="/" className="logo"><img src={logo} alt="Logo" /></Link>
                     <ul className="navMenu">
                         <Link to="/">Home</Link>
@@ -23,11 +37,5 @@ const Navbar = () => {
         </>
     )
 }
-
-const toggleBackground = () => {
-    console.log(window.scrollY)
-}
-
-window.addEventListener('scroll', toggleBackground())
 
 export default Navbar
