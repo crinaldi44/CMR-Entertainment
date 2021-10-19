d548dc8a4e321c844971c00e38ec39afdc070337
crinaldi44 <91025658+crinaldi44@users.noreply.github.com>
2021-10-19 18:50:04 -0400

    Added ServicesVideos & NavBar translation on scrollevent

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './Services.css'
 import Banner from '../components/Banner'
 import ServicesCatalogue from '../components/ServicesCatalogue'
+import ServicesVideos from '../components/ServicesVideos'
 
 
 const Services = () => {
@@ -9,6 +10,7 @@ const Services = () => {
         <div className="services__container">
             <Banner title="OUR SERVICES" details="CMR Entertainment is proud to provide a level of high-tech and advanced level services that are sure to make your event memorable!"/>
             <ServicesCatalogue />
+            <ServicesVideos />
         </div>
     )
 }
