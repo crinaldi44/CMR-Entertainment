442bb091f52c039e89da2d4353c849251784a6de
crinaldi44 <91025658+crinaldi44@users.noreply.github.com>
2021-11-16 16:26:56 -0500

    Remove montage
    Removed montage per client request

--- src/components/services/ServicesCatalogue.js
+++ src/components/services/ServicesCatalogue.js
@@ -5,10 +5,9 @@ import Photo1 from '../../images/services/photo-booth.png'
 import Photo2 from '../../images/services/photography.png'
 import Photo3 from '../../images/services/videography.png'
 import Photo4 from '../../images/services/lighting.png'
-import Photo5 from '../../images/services/live-video.png'
-import Photo6 from '../../images/services/media-montage.png'
-import Photo7 from '../../images/services/karaoke.png'
-import Photo8 from '../../images/services/event-planning.png'
+import Photo5 from '../../images/services/media-montage.png'
+import Photo6 from '../../images/services/karaoke.png'
+import Photo7 from '../../images/services/event-planning.png'
 import './ServicesCatalogue.css'
 
 const ServicesCatalogue = () => {
@@ -28,10 +27,9 @@ const ServicesCatalogue = () => {
                     <Card title="PHOTOGRAPHY" subtitle="Professional-grade cuts including a digital photo album. Each photo is processed before you receive proofs."image={Photo2}/>
                     <Card title="VIDEOGRAPHY" subtitle="Professionally-edited video including after-effects and post processing." image={Photo3}/>
                     <Card title="LIGHTING" subtitle="Set the mood and the tone for your event. Uplighting, stage-style lighting, dance-floor lighting and more." image={Photo4}/>
-                    <Card title="LIVE VIDEO" subtitle="Stream your event live or exclusively to your virtual guests." image={Photo5}/>
-                    <Card title="MEDIA MONTAGE" subtitle="Receive a custom directed and edited short video for your event consisting of professionally shot photo or video." image={Photo6}/>
-                    <Card title="KARAOKE" subtitle="Lyrics to your favorite songs will be displayed on a plasma screen TV. Makes for a great sing-along event!" image={Photo7}/>
-                    <Card title="EVENT PLANNING" subtitle="Professionally-curated events make the cut! We will architect the moment of your lifetime." image={Photo8}/>
+                    <Card title="MEDIA MONTAGE" subtitle="Receive a custom directed and edited short video for your event consisting of professionally shot photo or video." image={Photo5}/>
+                    <Card title="KARAOKE" subtitle="Lyrics to your favorite songs will be displayed on a plasma screen TV. Makes for a great sing-along event!" image={Photo6}/>
+                    <Card title="EVENT PLANNING" subtitle="Professionally-curated events make the cut! We will architect the moment of your lifetime." image={Photo7}/>
                 </div>
         </div>
     )
