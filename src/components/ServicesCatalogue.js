a15c4e8f630faf88b41e8a5ac30a4e3fa69b85c4
crinaldi44 <91025658+crinaldi44@users.noreply.github.com>
2021-11-16 10:51:17 -0500

    Restyle Services Page
    Restyled to more refined, professional design

--- src/components/ServicesCatalogue.js
+++ src/components/ServicesCatalogue.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import Card from '../components/Card'
-import Photo from '../images/services/dj.jpeg'
-import Photo1 from '../images/services/photo-booth.jpeg'
-import Photo2 from '../images/services/photography.jpeg'
-import Photo3 from '../images/services/videography.jpeg'
-import Photo4 from '../images/services/lighting.jpeg'
-import Photo5 from '../images/services/live-video.jpeg'
+import Photo from '../images/services/dj.png'
+import Photo1 from '../images/services/photo-booth.png'
+import Photo2 from '../images/services/photography.png'
+import Photo3 from '../images/services/videography.png'
+import Photo4 from '../images/services/lighting.png'
+import Photo5 from '../images/services/live-video.png'
 import './ServicesCatalogue.css'
 import Button from './Button'
 import { Link } from 'react-router-dom'
@@ -14,11 +14,6 @@ const ServicesCatalogue = () => {
 
     return (
         <div id="container" className="services__container__sub">
-                <div className="services__container__sub__description">
-                    <h2>What We Do</h2>
-                    <p>We provide a host of professional entertainment services and can accommodate any event! Mix and match services to make your event complete!</p>
-                    <Link to='/services/'><Button text="LEARN MORE"/></Link>
-                </div>
                 <div className="services__container__catalogue">
                     <Card title="DISC JOCKEY" subtitle="We provide the music and the energy all in one. We are experienced in all genres of music and will tailor our selection to yourself and your guests." image={Photo}/>
                     <Card title="PHOTO BOOTH" subtitle="Receive snapshots for your guests and a digital photo album for yourself. You'll get the choice between boardwalk and open-style booths."image={Photo1}/>
@@ -26,6 +21,11 @@ const ServicesCatalogue = () => {
                     <Card title="VIDEOGRAPHY" subtitle="Professionally-edited video including after-effects and post processing." image={Photo3}/>
                     <Card title="LIGHTING" subtitle="Set the mood and the tone for your event. Uplighting, stage-style lighting, dance-floor lighting and more." image={Photo4}/>
                     <Card title="LIVE VIDEO" subtitle="Stream your event live or exclusively to your virtual guests." image={Photo5}/>
+                </div>
+                <div className="services__container__sub__description">
+                    <h2>What We Do</h2>
+                    <p>We provide a host of professional entertainment services and can accommodate any event! Mix and match services to make your event complete!</p>
+                    <Link to='/services/'><Button text="LEARN MORE"/></Link>
                 </div>
         </div>
     )
--- src/pages/Services.js
+++ src/pages/Services.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './Services.css'
 import Banner from '../components/Banner'
 import ServicesCatalogue from '../components/ServicesCatalogue'
+import ServicesBanner from '../images/banner/services.png'
 
 
 const Services = () => {
     return (
         <div className="services__container">
-            <Banner title="OUR SERVICES" details="CMR Entertainment is proud to provide a variety of professional services that are sure to make your event memorable!"/>
+            <Banner title="OUR SERVICES" details="CMR Entertainment is proud to provide a variety of professional services that are sure to make your event memorable!"
+            image={ServicesBanner}/>
             <ServicesCatalogue />
         </div>
     )
