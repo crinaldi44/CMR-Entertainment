b4d3d5e4c078e41e7d88f2fcb9ae354f8b2c35aa
crinaldi44 <91025658+crinaldi44@users.noreply.github.com>
2021-12-13 22:53:22 -0500

    Update ContactForm
    ContactForm linked with DJEventPlanner backend, added FontAwesome icons to form input labels

--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -16,26 +16,25 @@ function ContactForm(props) {
     }
 
     return (
-        <form className='form__container' method='post' action='https://cmrentertainmentplanning.com/contactus.asp'>
+        <form className='form__container' method={'post'} action='https://cmrentertainmentplanning.com/contactus.asp'>
             
-            <div className='form__header'><h1>Contact Form</h1></div>
+            <div className='form__header'>
+                <h1>Contact Form</h1>
+            </div>
             <div className='form__content'>
             <h2>Contact Info</h2>
-                <div className='form__entry'><label>First Name*</label>
-                        <input className='form__input' required={true}></input>
+                <div className='form__entry'><label><i class='fa fa-user'></i>Full Name*</label>
+                        <input className='form__input' name="full_name" required={true} placeholder='Full name'></input>
                 </div>
-                <div className='form__entry'><label>Last Name*</label>
-                        <input className='form__input' required={true}></input>
+                <div className='form__entry'><label><i class='fa fa-envelope'></i>Email*</label>
+                        <input className='form__input' name="email" type={'email'} required={true} placeholder='Email address'></input>
                 </div>
-                <div className='form__entry'><label>Email*</label>
-                        <input className='form__input' required={true}></input>
-                </div>
-                <div className='form__entry'><label>Phone Number</label>
-                        <input className='form__input' ></input>
+                <div className='form__entry'><label><i class='fa fa-phone'></i>Phone Number</label>
+                        <input className='form__input' type={'tel'} name="question_1" placeholder='Phone number'></input>
                 </div>
             <h2>Event Info</h2>
                 <div className='form__entry'>
-                        <label>Type of Event*</label>
+                        <label><i class='fa fa-book'></i>Type of Event*</label>
                 <select required={true} name={"question_3"}>
                         <option>Wedding</option>
                         <option>Bat/Bar Mitzvah</option>
@@ -46,33 +45,19 @@ function ContactForm(props) {
                 </select>
                 </div>
             <div className='form__entry'>
-                <label>If other, please specify:</label>
-                <input></input>
+                <label><i class='fa fa-pencil'></i>If other, please specify:</label>
+                <input className='form__input' name='question_4' placeholder='Details'></input>
             </div>
-                    <label>Month of Event</label>
-                        <select>
-                                <option>January</option>
-                                <option>February</option>
-                                <option>March</option>
-                                <option>April</option>
-                                <option>May</option>
-                                <option>June</option>
-                                <option>July</option>
-                                <option>August</option>
-                                <option>September</option>
-                                <option>October</option>
-                                <option>November</option>
-                                <option>December</option>
-                        </select>
-            <label>Day of Event</label>
-            <input></input>
-            <label>Year of Event</label>
-            <input></input>
+            <div className='form__entry'>
+                <label><i class='fa fa-calendar'></i>Date of Event</label>
+                <input className='form__input' type={'date'} name='question_2'></input>
+            </div>
                 <label>Extra info for your event:</label>
-                <textarea className="form__textarea"></textarea>
+                <textarea className="form__textarea" name="message" placeholder='Please enter any miscellaneous info that you feel would aid us in your contact request.'></textarea>
                 <p>Please allow up to 48 hours for your response time. We gladly appreciate your request for more information!</p>
             <div style={submitButton}>
             <Button text='SUBMIT'/>
+            <input type="hidden" name="showlogo" value=""></input><input type="hidden" name="action" value="process"></input><input type="hidden" name="djidnumber" value="10512"></input><input type="hidden" name="source" value=''></input>
             </div>
             </div>
         </form>
